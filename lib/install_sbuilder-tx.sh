a47b8aa29e55175bef08cc31ff2da98883382487
Alexey <badperson483@gmail.com>
2019-02-19 19:06:28 +0300

    Minor commit

--- lib/install_sbuilder-tx.sh
+++ lib/install_sbuilder-tx.sh
@@ -2,8 +2,8 @@ cd ~
 git clone https://github.com/Pix-head/stealBuilder
 cd stealBuilder
 pip download pyinstaller
-tar -xzf Downloaded_file.tar.gz
-cd Downloaded_file
+tar -xzf PyInstaller*.tar.gz
+cd PyInstaller*
 sed -i'' -e 's#"/usr/tmp"#"/data/data/com.termux/files/usr/tmp"#g' bootloader/src/pyi_utils.c
 CFLAGS="-I/data/data/com.termux/files/usr/include/libandroid-support" LDFLAGS="-landroid-support" pip install .
 cd ..
